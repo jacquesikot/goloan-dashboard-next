6992796679726b13722feff29dcbda31d555c9aa
Jacques Ikot <jacquesikot@gmail.com>
2021-04-16 23:56:59 +0100

    fix: add document functions in useEffect hook to fix document not defined issue

---
+++ src/components/NavBar/useNavBar.ts
@@ -0,0 +1,43 @@
+import { useEffect } from 'react';
+
+const useNavBar = () => {
+  useEffect(() => {
+    showNavbar('header-toggle', 'nav-bar', 'body-pd', 'header');
+    /*===== LINK ACTIVE  =====*/
+    const linkColor = document.querySelectorAll('.nav__link');
+
+    function colorLink() {
+      if (linkColor) {
+        linkColor.forEach((l) => l.classList.remove('active'));
+        this.classList.add('active');
+      }
+    }
+    linkColor.forEach((l) => l.addEventListener('click', colorLink));
+  }, []);
+
+  /*===== SHOW NAVBAR  =====*/
+  const showNavbar = (toggleId, navId, bodyId, headerId) => {
+    const toggle =
+        typeof window !== 'undefined' && document.getElementById(toggleId),
+      nav = typeof window !== 'undefined' && document.getElementById(navId),
+      bodypd = typeof window !== 'undefined' && document.getElementById(bodyId),
+      headerpd =
+        typeof window !== 'undefined' && document.getElementById(headerId);
+
+    // Validate that all variables exist
+    if (toggle && nav && bodypd && headerpd) {
+      toggle.addEventListener('click', () => {
+        // show navbar
+        nav.classList.toggle('show');
+        // change icon
+        toggle.classList.toggle('bx-x');
+        // add padding to body
+        bodypd.classList.toggle('body-pd');
+        // add padding to header
+        headerpd.classList.toggle('body-pd');
+      });
+    }
+  };
+};
+
+export default useNavBar;
